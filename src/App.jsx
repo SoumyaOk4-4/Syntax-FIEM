486e547d727d584e70c728dddffce66bc5fe2409
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2025-03-13 00:43:52 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  //loading
+  useEffect(() => {
+    const delayTime = 3000;
+    const delayLoading = setTimeout(() => {
+      setLoading(false);
+    }, delayTime);
+
+    return () => clearTimeout(delayLoading);
+  }, []);
+
+  //did you knows
+  const dyk = "Did You Know:";
+  const DYK_arr = [
+    dyk + " hello", 
+    dyk + " hi", 
+    dyk + " bye", 
+    dyk + " ok"
+  ];
+
+  //did you know random function
+  function getRandom_DYK(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  return (
+    <div className="App">
+      {loading ? ( //loading...
+        <center>
+          <h4 className="did-you-know">{getRandom_DYK(DYK_arr)}</h4>
+        </center>
+      ) : (
+        <>
+          <h1>SYNTAX</h1>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
