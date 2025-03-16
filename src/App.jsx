9748e14c17b70a1c34c5818090c2fdcbe3c2e390
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2025-03-16 21:17:02 +0530

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
 //libs
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+
+//routes
+function Home() {
+  return (
+    <>
+      <h1>home page</h1>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <h1>about</h1>
+    </>
+  );
+}
+
+function Contact() {
+  return (
+    <>
+      <h1>contact</h1>
+    </>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -17,12 +53,7 @@ function App() {
 
   //did you knows
   const dyk = "Did You Know:";
-  const DYK_arr = [
-    dyk + " hello", 
-    dyk + " hi", 
-    dyk + " bye", 
-    dyk + " ok"
-  ];
+  const DYK_arr = [dyk + " hello", dyk + " hi", dyk + " bye", dyk + " ok"];
 
   //did you know random function
   function getRandom_DYK(array) {
@@ -32,17 +63,28 @@ function App() {
 
   return (
     <div className="App">
-      {loading ? ( 
+      {loading ? (
         //loading screen
         <center>
-          <br /><br /><br />
+          <br />
+          <br />
+          <br />
           <h4 className="did-you-know">{getRandom_DYK(DYK_arr)}</h4>
-          <br /><br />
+          <br />
+          <br />
           <div className="loadingscreen"></div>
         </center>
       ) : (
         <>
           <h1 className="aaa-temp">SYNTAX</h1>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Router>
         </>
       )}
     </div>
