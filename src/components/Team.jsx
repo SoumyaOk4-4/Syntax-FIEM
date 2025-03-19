a5ad5b4dcb3eacc83b07a7412800736499e9ead0
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2025-03-19 20:55:43 +0530

    team update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,46 +1,64 @@
+//contents
+import soumya from "../images/team_pics/soumya.jpg";
+import ayan from "../images/team_pics/ayan.jpg";
+import mrigank from "../images/team_pics/mrigank.jpg";
+import sagufta from "../images/team_pics/sagufta.jpg";
+import aniket from "../images/team_pics/aniket.jpg";
+import ankush from "../images/team_pics/ankush.jpg";
+import shubhangi from "../images/team_pics/shubhangi.jpg";
+import deboshreya from "../images/team_pics/deboshreya.jpg";
+
 function Team() {
   return (
     <>
       <div className="fadein_fadeout web-content">
         <div>
-          name = Ankush Samanta <br />
-          linkedin = https://www.linkedin.com/in/ankush-samanta-804403295/ <br />
-          github = https://github.com/MrFantastico007
+          <img src={ankush} alt="Ankush Samanta" /><br />
+          Ankush Samanta <br />
+          <a href="https://www.linkedin.com/in/ankush-samanta-804403295/" className="profile-pic">LinkedIn</a> <br />
+          <a href="https://github.com/MrFantastico007">GitHub</a>
         </div>
         <div>
-          name = Mrigank Sen <br />
-          linkedin = https://www.linkedin.com/in/mrigank-sen-ba843827a/ 
-          github = https://github.com/OrangeCatKekw <br />
+          <img src={mrigank} alt="Mrigank Sen" />
+          Mrigank Sen <br />
+          <a href="https://www.linkedin.com/in/mrigank-sen-ba843827a/">LinkedIn</a> <br />
+          <a href="https://github.com/OrangeCatKekw">GitHub</a> <br />
         </div>
         <div>
-          name = Sagufta Sinha <br />
-          linkedin = https://www.linkedin.com/in/sagufta-sinha-a03491295/ <br />
-          github = https://github.com/Sagufta1
+          <img src={sagufta} alt="Sagufta Sinha" />
+          Sagufta Sinha <br />
+          <a href="https://www.linkedin.com/in/sagufta-sinha-a03491295/">LinkedIn</a> <br />
+          <a href="https://github.com/Sagufta1">GitHub</a>
         </div>
         <div>
-          name = Soumya Chakraborty <br />
-          linkedin = https://www.linkedin.com/in/soumya-chakraborty-122388225/ <br />
-          github = https://github.com/SoumyaOk4-4
+          <img src={soumya} alt="Soumya Chakraborty" />
+          Soumya Chakraborty <br />
+          <a href="https://www.linkedin.com/in/soumya-chakraborty-122388225/">LinkedIn</a> <br />
+          <a href="https://github.com/SoumyaOk4-4">GitHub</a>
         </div>
         <div>
-          name = Ayan Purkait <br />
-          linkedin = https://www.linkedin.com/in/ayan-purkait-615464289/ <br />
-          github = https://github.com/JustChillinBro92
+          <img src={ayan} alt="Ayan Purkait" />
+          Ayan Purkait <br />
+          <a href="https://www.linkedin.com/in/ayan-purkait-615464289/">LinkedIn</a> <br />
+          <a href="https://github.com/JustChillinBro92">GitHub</a>
         </div>
         <div>
-          name = Aniket Nandi <br />
-          linkedin = https://www.linkedin.com/in/aniket-nandi%F0%9F%A7%91%E2%80%8D%F0%9F%9A%80-9620a4216/ <br />
-          github = https://github.com/Optimus1o1
+          <img src={aniket} alt="Aniket Nandi" />
+          Aniket Nandi <br />
+          <a href="https://www.linkedin.com/in/aniket-nandi%F0%9F%A7%91%E2%80%8D%F0%9F%9A%80-9620a4216/">LinkedIn</a> <br />
+          <a href="https://github.com/Optimus1o1">GitHub</a>
         </div>
         <div>
-          name = Deboshreya Ganguly <br />
-          linkedin = https://www.linkedin.com/in/deboshreya-ganguly-73b813224/ <br />
-          github = https://github.com/Deboshreya0603
+          <img src={deboshreya} alt="Deboshreya Ganguly" />
+          Deboshreya Ganguly <br />
+          <a href="https://www.linkedin.com/in/deboshreya-ganguly-73b813224/">Linked In</a> <br />
+          <a href="https://github.com/Deboshreya0603">GitHub</a>
         </div>
         <div>
-          name = Subhangi Bandyopadhyay <br />
-          linkedin = https://www.linkedin.com/in/subhangi-bandyopadhyay-9196132a5/ <br />
-          github = https://github.com/Subhangi-14
+          <img src={shubhangi} alt="Subhangi Bandyopadhyay" />
+          Subhangi Bandyopadhyay <br />
+          <a href="https://www.linkedin.com/in/subhangi-bandyopadhyay-9196132a5/">LinkedIn</a> <br />
+          <a href="https://github.com/Subhangi-14">GitHub</a>
         </div>
       </div>
     </>
