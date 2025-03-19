b0fa42e4d6da4c8e3e259f5c1c0d6ed49a45b295
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2025-03-19 20:33:15 +0530

    lil fix 1.0

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,45 +1,45 @@
 function Team() {
   return (
     <>
-            <div className="fadein_fadeout web-content">
+      <div className="fadein_fadeout web-content">
         <div>
           name = Ankush Samanta <br />
-          linkedin=https://www.linkedin.com/in/ankush-samanta-804403295/
+          linkedin = https://www.linkedin.com/in/ankush-samanta-804403295/ <br />
           github = https://github.com/MrFantastico007
         </div>
         <div>
           name = Mrigank Sen <br />
-          linkedin=https://www.linkedin.com/in/mrigank-sen-ba843827a/
-          github = https://github.com/OrangeCatKekw
+          linkedin = https://www.linkedin.com/in/mrigank-sen-ba843827a/ 
+          github = https://github.com/OrangeCatKekw <br />
         </div>
         <div>
           name = Sagufta Sinha <br />
-          linkedin=https://www.linkedin.com/in/sagufta-sinha-a03491295/
+          linkedin = https://www.linkedin.com/in/sagufta-sinha-a03491295/ <br />
           github = https://github.com/Sagufta1
         </div>
         <div>
           name = Soumya Chakraborty <br />
-          linkedin = https://www.linkedin.com/in/soumya-chakraborty-122388225/
+          linkedin = https://www.linkedin.com/in/soumya-chakraborty-122388225/ <br />
           github = https://github.com/SoumyaOk4-4
         </div>
         <div>
           name = Ayan Purkait <br />
-          linkedin = https://www.linkedin.com/in/ayan-purkait-615464289/
+          linkedin = https://www.linkedin.com/in/ayan-purkait-615464289/ <br />
           github = https://github.com/JustChillinBro92
         </div>
         <div>
           name = Aniket Nandi <br />
-          linkedin = https://www.linkedin.com/in/aniket-nandi%F0%9F%A7%91%E2%80%8D%F0%9F%9A%80-9620a4216/
+          linkedin = https://www.linkedin.com/in/aniket-nandi%F0%9F%A7%91%E2%80%8D%F0%9F%9A%80-9620a4216/ <br />
           github = https://github.com/Optimus1o1
         </div>
         <div>
           name = Deboshreya Ganguly <br />
-          linkedin = https://www.linkedin.com/in/deboshreya-ganguly-73b813224/
+          linkedin = https://www.linkedin.com/in/deboshreya-ganguly-73b813224/ <br />
           github = https://github.com/Deboshreya0603
         </div>
         <div>
           name = Subhangi Bandyopadhyay <br />
-          linkedin = https://www.linkedin.com/in/subhangi-bandyopadhyay-9196132a5/
+          linkedin = https://www.linkedin.com/in/subhangi-bandyopadhyay-9196132a5/ <br />
           github = https://github.com/Subhangi-14
         </div>
       </div>
