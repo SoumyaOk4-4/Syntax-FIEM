13284b4be92bc4fcdcffe5c36c273c60a70171be
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2025-03-19 20:29:37 +0530

    team update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,6 +4,48 @@ function Team() {
       <br />
       <br />
       <h1 className="fadein_fadeout">team member page</h1>
+      <div className="fadein_fadeout web-content">
+        <div>
+          name = Ankush Samanta <br />
+          linkedin=https://www.linkedin.com/in/ankush-samanta-804403295/
+          github = https://github.com/MrFantastico007
+        </div>
+        <div>
+          name = Mrigank Sen <br />
+          linkedin=https://www.linkedin.com/in/mrigank-sen-ba843827a/
+          github = https://github.com/OrangeCatKekw
+        </div>
+        <div>
+          name = Sagufta Sinha <br />
+          linkedin=https://www.linkedin.com/in/sagufta-sinha-a03491295/
+          github = https://github.com/Sagufta1
+        </div>
+        <div>
+          name = Soumya Chakraborty <br />
+          linkedin = https://www.linkedin.com/in/soumya-chakraborty-122388225/
+          github = https://github.com/SoumyaOk4-4
+        </div>
+        <div>
+          name = Ayan Purkait <br />
+          linkedin = https://www.linkedin.com/in/ayan-purkait-615464289/
+          github = https://github.com/JustChillinBro92
+        </div>
+        <div>
+          name = Aniket Nandi <br />
+          linkedin = https://www.linkedin.com/in/aniket-nandi%F0%9F%A7%91%E2%80%8D%F0%9F%9A%80-9620a4216/
+          github = https://github.com/Optimus1o1
+        </div>
+        <div>
+          name = Deboshreya Ganguly <br />
+          linkedin = https://www.linkedin.com/in/deboshreya-ganguly-73b813224/
+          github = https://github.com/Deboshreya0603
+        </div>
+        <div>
+          name = Subhangi Bandyopadhyay <br />
+          linkedin = https://www.linkedin.com/in/subhangi-bandyopadhyay-9196132a5/
+          github = https://github.com/Subhangi-14
+        </div>
+      </div>
     </>
   );
 }
