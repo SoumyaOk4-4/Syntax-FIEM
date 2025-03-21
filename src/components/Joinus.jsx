4a35f7e59c923fc4b625899470b4ba7740fef732
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2025-03-21 14:52:41 +0530

    screen for older laptop fixed

--- src/components/Joinus.jsx
+++ src/components/Joinus.jsx
@@ -3,7 +3,7 @@ function Joinus() {
     <>
       <h1>Join Us</h1>
       <br /><br /><br /><br /><br /><br /><br />
-      <a href="https://forms.gle/soH7n2CjLckWHyEE9" target="_blank" rel="noopener noreferrer">Volunteer Form</a>
+      <a href="https://forms.gle/soH7n2CjLckWHyEE9" target="_blank" rel="noopener noreferrer" className="vol-form">Volunteer Form</a>
     </>
   );
 }
