a028ad7dce165636697cf15a6d07b4279d9cd6db
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2025-03-21 19:29:07 +0530

    404 color change

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,7 +6,7 @@ function Error() {
         {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
         <marquee><span style={{color:"gray", fontSize:"3rem", fontFamily:"Font2"}}>Nothing to see here!🤷‍♂️</span></marquee>
         <br /><br /><br /><br /><br /><br /><br />
-        <h1><a href="/" style={{color:"gray", fontSize:"2rem", fontFamily:"Font2"}}>👉 Return to Home Page 👈</a></h1>
+        <h1><a href="/" style={{color:"#e7a529", fontSize:"2rem", fontFamily:"Font2"}}>👉 Return to Home Page 👈</a></h1>
       </>
     );
   }
