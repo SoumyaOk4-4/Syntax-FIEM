9e0147fce249ebff6fd2004e9ab363c1afc747de
JustChillinBro92 <ayanpurkait11@gmail.com>
2025-04-06 22:02:56 +0530

    ui fix

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -150,6 +150,29 @@ function Team() {
           </div>
 
           <div className="team-member">
+            <img src={ayan} alt="Ayan Purkait" className="profile-pic" />
+            <br />
+            Ayan Purkait
+            <div className="Job-Role">Support Dev</div>
+            <div className="socials">
+              <a
+                href="https://www.linkedin.com/in/ayan-purkait-615464289/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin className="team-socials linkedin" />
+              </a>
+              <a
+                href="https://github.com/JustChillinBro92"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub className="team-socials github" />
+              </a>
+            </div>
+          </div>
+
+          <div className="team-member">
             <img src={sagufta} alt="Sagufta Sinha" className="profile-pic" />
             <br />
             Sagufta Sinha
@@ -249,28 +272,6 @@ function Team() {
             </div>
           </div>
 
-          <div className="team-member">
-            <img src={ayan} alt="Ayan Purkait" className="profile-pic" />
-            <br />
-            Ayan Purkait
-            <div className="Job-Role">Support Dev</div>
-            <div className="socials">
-              <a
-                href="https://www.linkedin.com/in/ayan-purkait-615464289/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedin className="team-socials linkedin" />
-              </a>
-              <a
-                href="https://github.com/JustChillinBro92"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaGithub className="team-socials github" />
-              </a>
-            </div>
-          </div>
           <div className="team-member">
             <img src={arnab} alt="Arnab Das" className="profile-pic" />
             <br />
